596823b05271892895f944d5abb194d95158f1f2
avi_singh <avi.v.singh@gmail.com>
2023-06-24 10:28:28 +0100

    Display page when valid entry is presented and display blank page when non existent entry is presented, markdown converter written

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
 
@@ -8,3 +9,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def markdown_conversion(md_file):
+    markdowner = markdown.Markdown()
+    md_content = util.get_entry(md_file)
+
+    if md_content is None:
+        return None
+    else:
+        return markdowner.convert(md_content)
+
+
+def entry(request, title):
+    if markdown_conversion(title) is None:
+        return render(request, 'encyclopedia/no_entry.html')
+    else:
+        return render(request, 'encyclopedia/entry.html')
